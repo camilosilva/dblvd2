d7fc1731bb52a03fdab45b7a76c85e1bded1d31d
Camilo Silva <silva.creative@gmail.com>
2013-12-19 01:07:40 -0500

    qa1 db schema added

---
+++ docs/qa1.dblvd.schema.sql
@@ -0,0 +1,15 @@
+/* Schema of DadBlvd2 */
+CREATE DATABASE qa1dadblvd
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_unicode_ci;
+
+drop table if exists roles;
+create table roles(
+  id int not null primary key auto_increment,
+  name varchar(20),
+  created_at datetime,
+  updated_at datetime
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
