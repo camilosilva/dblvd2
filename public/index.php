3cd43bacdf6ae6f52506ba6ae9be40b8d2f2d073
Camilo Silva <silva.creative@gmail.com>
2013-12-20 03:43:24 -0500

    index modified

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 //Sets initial app settings
-include __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR,array('dblvdapp','config','bootstrap.php'));
+include __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR,array('DblvdApp','Config','bootstrap.php'));
 //Handle requests on Main Controller
-include SERVICES . DS . implode(DS , array( 'controllers' ,  'MainController.php'));
+include SERVICES . DS . implode(DS , array( 'Controllers' ,  'MainController.php'));
